eb319028ea90fa4d519927bd34b13cb28d0f6cb5
chuang hao wei <spirit0919hd@gmail.com>
2024-10-22 17:56:53 +0800

    Create wordle game structure and functions: GameBoard, KeyBoard
    structure:
    APP uses Header, MainBoard, Extend
    MainBoard: GameBoard, KeyBoard
    
    Function:
    GameBoard: Create a 6x5 game grid, display current guesses and historical guesses, and display colors (green, yellow, gray) based on guess results
    
    KeyBoard: Add a virtual keyboard interface, support clicking to enter letters, and update the keyboard color according to the letters used

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import MainBoard from "./components/MainBoard";
+import Extend from "./components/Extend";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainBoard />
+      <Extend />
+    </div>
+  );
+}
+
+export default App;
